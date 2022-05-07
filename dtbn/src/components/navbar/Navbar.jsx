6947ca1fa94adea0c996ff6d86001b1056ec05f9
Greg Hayes <gthelight@gmail.com>
2022-05-07 12:37:22 -0400

    New commit for folder structure

---
+++ dtbn/src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {RiMenu3Line, RiCloseLin} from 'react-icons/ri'
+
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='dtbn__navbar'>
+      <div className="dtbn__navbar-links">
+        <div className="dtbn__navbar-links_logo">
+          <img src='#' alt="logo" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ dtbn/src/containers/whatDTBN/WhatDTBN.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './whatDTBN.css'
+
+const WhatDTBN = () => {
+  return (
+    <div>WhatDTBN</div>
+  )
+}
+
+export default WhatDTBN
