6721ccc4ade08fb1980c8b35d092fd43c07bce90
Greg Hayes <gthelight@gmail.com>
2022-05-07 16:39:09 -0400

    Styles for Navbar

--- dtbn/src/components/navbar/Navbar.jsx
+++ dtbn/src/components/navbar/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-import {RiMenu3Line, RiCloseLin} from 'react-icons/ri'
+import {useState} from 'react'
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri'
 import logo from '../../assets/logo.svg'
 import './navbar.css'
 
+const Menu = () => (
+  <>          
+    <p><a href="#home">Home</a></p>
+    <p><a href="#wdtbn">What is DTBN?</a></p>
+    <p><a href="#possibility">Open AI</a></p>
+    <p><a href="#features">Case Studies</a></p>
+    <p><a href="#blog">Library</a></p>
+  </>
+)
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="dtbn__navbar">
       <div className="dtbn__navbar-links">
@@ -11,13 +24,30 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="dtbn__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wdtbn">What is DTBN?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+          <Menu />
         </div>
       </div>
+      <div className="dtbn__navbar-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+        </div>
+        <div className="dtbn__navbar-menu">
+          {toggleMenu 
+          ? <RiCloseLine color='#fff' size={27} onClick={() => {setToggleMenu(false)}}  />
+          : <RiMenu3Line color='#fff' size={27} onClick={() => {setToggleMenu(true)}} />
+        }
+        {toggleMenu && (
+          <div className="dtbn__navbar-menu_container scale-up-center">
+            <div className="dtbn__navbar-menu_container-links">
+              <Menu/>
+              <div className="dtbn__navbar-menu_container-links-sign">
+              <p>Sign In</p>
+              <button type="button">Sign Up</button>
+        </div>
+            </div>
+          </div>
+        )}
+        </div>
     </div>
   )
 }
