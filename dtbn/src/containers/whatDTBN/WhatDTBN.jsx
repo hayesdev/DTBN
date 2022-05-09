ec4ef29ca128e297e8c700006fb61b181084367f
Greg Hayes <gthelight@gmail.com>
2022-05-09 13:10:02 -0400

    Styels 'what' component

--- dtbn/src/containers/whatDTBN/WhatDTBN.jsx
+++ dtbn/src/containers/whatDTBN/WhatDTBN.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Feature from '../../components/feature/Feature'
 import './whatDTBN.css'
 
 const WhatDTBN = () => {
   return (
-    <div>WhatDTBN</div>
+    <div className="dtbn__whatdtbn section__margin" id='whatdtbn'>
+      <div className="dtbn__whatdtbn-feature">
+        <Feature />  
+      </div>
+      <div className="dtbn__whatdtbn-heading">
+        <h1 className="gradient__text">
+          The Possibilities Are Beyond Your Imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="dtbn__whatdtbn-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
